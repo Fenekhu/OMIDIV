537be5ecccf5e513309274ec9d8bc248da0c67a1
Fenekhu <firebirdspiral@pm.me>
2026-01-26 12:52:08 -0600

    use NLayer for cross-platform audio

--- Assets/AudioImporter/Scripts/NAudioImporter.cs
+++ Assets/AudioImporter/Scripts/NAudioImporter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NAudio.Wave;
+using NLayer.NAudioSupport;
 using UnityEngine;
 
 /// <summary>
@@ -17,8 +18,9 @@ public class NAudioImporter : DecoderImporter
         {
             if (!uri.IsFile)
                 throw new FormatException("NAudioImporter does not support URLs");
-
-            waveStream = new AudioFileReader(uri.LocalPath);
+            
+            var builder = new Mp3FileReader.FrameDecompressorBuilder(wf => new Mp3FrameDecompressor(wf));
+            waveStream = new Mp3FileReader(uri.LocalPath, builder);
             sampleProvider = waveStream.ToSampleProvider();
         }
         catch (Exception e)
